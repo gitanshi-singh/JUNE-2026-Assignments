50f15d268513a7be85508c7457f2ae556e8261f3
gitanshi-singh <gitanshi.college097@gmail.com>
2026-06-23 13:20:33 +0530

    04.06.2026

---
+++ Q1_examFormException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// WAP to check whether you have applied/submitted your exam form before deadline or not.
+//Custom defined exceptions
+namespace JUNE_040626
+{
+    class DeadlineException : Exception
+    {
+        public DeadlineException(string message) : base(message)
+        {
+        }
+    }
+    internal class Q1_examFormException
+    {
+        public static void Run()
+        {
+            DateTime deadline = new DateTime(2026, 7, 15);
+
+            Console.Write("Enter submission date (yyyy-mm-dd): ");
+            DateTime submissionDate = Convert.ToDateTime(Console.ReadLine());
+
+            try
+            {
+                if (submissionDate > deadline)
+                {
+                    throw new DeadlineException("Exam form submitted after the deadline.");
+                }
+                Console.WriteLine("Exam form submitted successfully");
+            }
+            catch (DeadlineException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Q2_cabBookingException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Create an application for cab booking. But the gps service fails or invalid location entered
+//Exception handling
+namespace JUNE_040626
+{
+    class GPSException : Exception
+    {
+        public GPSException(string message) : base(message) { }
+    }
+
+    class InvalidLocationException : Exception
+    {
+        public InvalidLocationException(string message) : base(message) { }
+    }
+    internal class Q2_cabBookingException
+    {
+        public static void Run()
+        {
+            try
+            {
+                Console.Write("Enter pickup location: ");
+                string location = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(location))
+                {
+                    throw new InvalidLocationException("Invalid location entered.");
+                }
+                Console.Write("Is GPS available? (yes/no): ");
+                string gps = Console.ReadLine();
+
+                if (gps.ToLower() == "no")
+                {
+                    throw new GPSException("GPS service failed.");
+                }
+
+                Console.WriteLine("Cab booked successfully.");
+            }
+            catch(InvalidLocationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(GPSException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
